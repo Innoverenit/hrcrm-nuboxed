5310fb3f1c837f8bcc133e857a384f6bcf350709
karisma <you@example.com>
2024-03-13 09:45:23 +0530

    karisma

--- src/Containers/Auth/ForgotPassword.js
+++ src/Containers/Auth/ForgotPassword.js
@@ -185,7 +185,7 @@ class ForgotPassword extends Component {
               >
                 <img
                   className="big-logo"
-                  src={FWLogo}
+                  src={FWLogo2}
                   style={{ width: 200 }}
                   alt="Tekorero logo"
                 />
