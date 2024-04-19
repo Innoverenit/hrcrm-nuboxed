ea238799fc97405428cc3303abb3dc942c12b0bc
Sangharsh <sangharshmishra16@gmail.com>
2024-04-19 12:54:46 +0530

    Sangharsh//Done

--- src/Config/Auth.js
+++ src/Config/Auth.js
@@ -1,8 +1,8 @@
-//Innoveren
-// const login_url = "https://develop.tekorero.com/employeePortal";
-// const base_url = "https://develop.tekorero.com/employeePortal/api/v1";
-// const base_url2 = "https://develop.tekorero.com/Erp";
-// const asses_url = "https://develop.tekorero.com/Assessment-0.0.1-SNAPSHOT";
+// Innoveren
+const login_url = "https://develop.tekorero.com/employeePortal";
+const base_url = "https://develop.tekorero.com/employeePortal/api/v1";
+const base_url2 = "https://develop.tekorero.com/Erp";
+const asses_url = "https://develop.tekorero.com/Assessment-0.0.1-SNAPSHOT";
 
 
 //Nubox
@@ -13,10 +13,10 @@
 
 
 // Cell Tech
-const login_url = "https://crm.celltechnologies.nl";
-const base_url = "https://crm.celltechnologies.nl/api/v1";
-const base_url2 = "https://erp.celltechnologies.nl";
-const asses_url = "https://crm.celltechnologies.nl/Assessment-0.0.1-SNAPSHOT";
+// const login_url = "https://crm.celltechnologies.nl";
+// const base_url = "https://crm.celltechnologies.nl/api/v1";
+// const base_url2 = "https://erp.celltechnologies.nl";
+// const asses_url = "https://crm.celltechnologies.nl/Assessment-0.0.1-SNAPSHOT";
 
 export { base_url, login_url, asses_url, base_url2 };
 
