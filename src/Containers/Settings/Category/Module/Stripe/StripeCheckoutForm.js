af244fcaf96a4e53a84386f811e4726591d55507
Dibya391 <dibyaranjan828@gmail.com>
2024-06-12 16:53:02 +0530

    drb13

---
+++ src/Containers/Settings/Category/Module/Stripe/StripeCheckoutForm.js
@@ -0,0 +1,71 @@
+import React, {useState,useEffect} from "react";
+import {Elements, ElementsConsumer} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+import CheckoutForm from "./CheckoutForm";
+// import {sendPaymentInfo} from "../../LeadsAction";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+const stripePromise = loadStripe('pk_test_51MHMyeLUPUCPutTHmfURN9aHilVqsmPDU8gQIt2nndD3iRrOwAobmFh567r34xIjzqSRexw6muNUuPGgYxBOZv1J00WHxEDkln');
+
+function StripeCheckoutForm(props) {
+  // useEffect(() => {
+  //   const multiplyTotal=total*100;
+  //   let data = {
+  //     amount:multiplyTotal,
+  //     requirementId:requirementId,
+  //     serviceId:props.serviceId,
+  //     currency:"EUR",
+  //   };
+  //   props.sendPaymentInfo(data);
+  // }, []);
+  // const total=props.setEditingLeadsCard.costPerLeadViewDTO.total
+  //    console.log("samnt",total)
+  // const options={clientSecret:props.paymentDetails.clientSecret}
+  // const requirementId=props.setEditingLeadsCard.requirementId
+  return (
+    <>
+   {/* {props.paymentDetails.clientSecret &&  */}
+    <Elements stripe={stripePromise} 
+    // options={options}
+    >
+      <ElementsConsumer>
+      {({ stripe, elements }) => (
+      <CheckoutForm
+      stripe={stripe}
+      elements={elements}
+       handleStripeModal={props.handleStripeModal}
+      //  total={total}
+      //  ConfirmedPayment={props.ConfirmedPayment}
+      //  serviceId={props.serviceId}
+      //  stripePaymentId={props.stripePaymentId}
+      //  requirementId={requirementId}
+      //  setEditingLeadsCard={props.setEditingLeadsCard}
+       />
+       )}
+       </ElementsConsumer>
+    </Elements>
+   {/* } */}
+   </>
+  );
+};
+
+const mapStateToProps = ({ leads,auth }) => ({
+  // paymentDetails: leads.paymentDetails,
+  // ConfirmedPayment: leads.ConfirmedPayment,
+  // serviceId:auth.serviceDetails.serviceId,
+  // showLeadsAllTable:leads.showLeadsAllTable,
+  // setEditingLeadsCard:leads.setEditingLeadsCard,
+  // stripePaymentId:leads.paymentDetails.stripePaymentId,
+});
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators(
+    {
+      // sendPaymentInfo
+    },
+    dispatch
+  );
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(StripeCheckoutForm)
+);
---
+++ src/Containers/Settings/Category/Module/Stripe/StripePaymentModal.js
@@ -0,0 +1,41 @@
+import React, { lazy, Suspense } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { withRouter } from "react-router-dom";
+import StripeCheckoutForm from "./StripeCheckoutForm";
+import { BundleLoader } from "../../../../../Components/Placeholder";
+import { StyledModal } from "../../../../../Components/UI/Antd";
+
+const StripePaymentModal = (props) => {
+  const { ...formProps } = props;
+  return (
+    <>
+      <StyledModal
+        title="Stripe"
+        visible={props.addStripeModal}
+        maskClosable={false}
+        destroyOnClose
+        onCancel={() => props.handleStripeModal(false)}
+        maskStyle={{ backgroundColor: "rgba(1, 30, 71,0.7)" }}
+        style={{ top: 40 }}
+        footer={null}
+      >
+        <Suspense fallback={<BundleLoader />}>
+         {/* <StripeCheckoutForm
+            handleStripeModal={props.handleStripeModal}
+           
+          />  */}
+          hii
+        </Suspense>
+      </StyledModal>
+    </>
+  );
+};
+const mapStateToProps = ({ leads, auth }) => ({
+});
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({}, dispatch);
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(StripePaymentModal)
+);
