afc953727e3f281d92481aef283f70382e71baf1
saumya-lapi <srjjack9@gmail.com>
2024-07-12 13:21:17 +0530

    saumya//done

--- src/Containers/Main/Club/ClubAction.js
+++ src/Containers/Main/Club/ClubAction.js
@@ -7,3 +7,30 @@ import Swal from "sweetalert2";
 
 export const setClubViewType = (viewType) => (dispatch) =>
     dispatch({ type: types.SET_CLUB_VIEW_TYPE, payload: viewType });
+
+export const getClubAlllist = (userId,pageNo,clubType) => (dispatch) => {
+ 
+    dispatch({
+      type: types.GET_CLUB_ALLLIST_REQUEST,
+    });
+    axios
+      .get(`${base_url}/investor/club/${userId}/${pageNo}/${clubType}`, {
+        headers: {
+          Authorization: "Bearer " + sessionStorage.getItem("token") || "",
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        dispatch({
+          type: types.GET_CLUB_ALLLIST_SUCCESS,
+          payload: res.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err.response);
+        dispatch({
+          type: types.GET_CLUB_ALLLIST_FAILURE,
+          payload: err,
+        });
+      });
+  };
