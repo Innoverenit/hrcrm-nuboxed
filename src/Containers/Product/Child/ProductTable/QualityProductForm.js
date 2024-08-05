6fc7589772c98b7b69f9678c8528d5e4da04cae2
Itisri <itisrichaudhury236>
2024-08-05 11:55:30 +0530

    By itisri

--- src/Containers/Product/Child/ProductTable/QualityProductForm.js
+++ src/Containers/Product/Child/ProductTable/QualityProductForm.js
@@ -143,9 +143,9 @@ useEffect(() => {
       >
         {isDragAndDropEnabled ? 'Disable Order Change' : 'Change Order'}
       </button>
-    <div className='flex justify-end sticky z-auto'>
-            <div className="rounded-lg m-5 p-2 w-[96%] overflow-auto shadow-[4px_0px_9px_3px_] shadow-[#a3abb980] bg-[#eaedf1]">
-                <div className="flex w-[95%] px-2 bg-transparent font-bold sticky top-0 z-10">
+    <div className='flex sticky z-auto'>
+            <div className="rounded m-1 p-1 w-[99%] overflow-auto shadow-[4px_0px_9px_3px_] shadow-[#a3abb980] bg-[#eaedf1]">
+                <div className="flex w-[99%] p-1 bg-transparent font-bold sticky  z-10">
                     <div className=""></div>
                     <div className="md:w-[22.12rem]"><FormattedMessage id="app.step" defaultMessage="Step" /></div>
                     <div className="md:w-[15.5rem]"><FormattedMessage id="app.serialno" defaultMessage="Serial No" /></div>
@@ -159,14 +159,14 @@ useEffect(() => {
                     return (
                         <div key={index}>
                             <div className="flex rounded-xl mt-2 bg-white h-12 items-center p-3">
-                                <div className="flex font-medium flex-col md:w-[36.1rem] max-sm:w-full">
+                                <div className="flex md:w-[36.1rem] max-sm:w-full">
                                     <div className="flex justify-between text-sm  font-semibold font-poppins">
                                         {item.qualityName}
                                     </div>
                                 </div>
 
-                                <div className="flex font-medium flex-col md:w-26 max-sm:justify-between w-full max-sm:flex-row">
-                                    <div className="font-normal text-[0.85rem]  font-poppins" style={{ marginLeft: "9em" }}>
+                                <div className="flex md:w-26 max-sm:justify-between w-full max-sm:flex-row">
+                                    <div className="  text-[0.85rem]  font-poppins" style={{ marginLeft: "9em" }}>
                                         {item.steps}
                                     </div>
                                 </div>
@@ -189,10 +189,10 @@ useEffect(() => {
                         ref={provided.innerRef}
                         {...provided.draggableProps}
                         {...provided.dragHandleProps}
-                        className="flex rounded-xl mt-2 bg-white h-12 items-center p-3"
+                        className="flex rounded mt-1 bg-white h-8 items-center p-1"
                       >
-                        <div className="flex font-medium flex-col md:w-[36.1rem] max-sm:w-full">
-                          <div className="flex justify-between text-sm font-semibold font-poppins">
+                        <div className="flex md:w-[36.1rem] max-sm:w-full">
+                          <div className="flex justify-between text-xs font-semibold font-poppins">
                             {/* {item.qualityName} */}
                             {editingIndex === index ? (
                 <Input
@@ -206,14 +206,14 @@ useEffect(() => {
                           </div>
                         </div>
 
-                        <div className="flex font-medium flex-col md:w-26 max-sm:justify-between w-full max-sm:flex-row">
-                          <div className="font-normal text-[0.85rem] font-poppins" style={{ marginLeft: "9em" }}>
+                        <div className="flex md:w-26 max-sm:justify-between w-full max-sm:flex-row">
+                          <div className="  text-[0.85rem] font-poppins" style={{ marginLeft: "9em" }}>
                             {item.steps}
                           </div>
                         </div>
 
-                        <div className="flex font-medium flex-col md:w-26 max-sm:justify-between w-full max-sm:flex-row">
-                          <div className="font-normal text-[0.85rem] font-poppins" style={{ marginLeft: "9em" }}>
+                        <div className="flex md:w-26 max-sm:justify-between w-full max-sm:flex-row">
+                          <div className="  text-[0.85rem] font-poppins" style={{ marginLeft: "9em" }}>
                             {/* {item.steps} */}
 
                             <MoveToggleQualityProduct 
@@ -229,8 +229,8 @@ useEffect(() => {
 
 
 
-                        <div className="flex font-medium flex-col md:w-26 max-sm:justify-between w-full max-sm:flex-row">
-                          <div className="font-normal text-[0.85rem] font-poppins" style={{ marginLeft: "9em" }}>
+                        <div className="flex md:w-26 max-sm:justify-between w-full max-sm:flex-row">
+                          <div className="  text-[0.85rem] font-poppins" style={{ marginLeft: "9em" }}>
                             {/* {item.steps} */}
 
                             {editingIndex === index ? (
@@ -246,8 +246,8 @@ useEffect(() => {
 
 
 
-                        <div className="flex font-medium flex-col md:w-26 max-sm:justify-between w-full max-sm:flex-row">
-                          <div className="font-normal text-[0.85rem] font-poppins" style={{ marginLeft: "9em" }}>
+                        <div className="flex md:w-26 max-sm:justify-between w-full max-sm:flex-row">
+                          <div className="  text-[0.85rem] font-poppins" style={{ marginLeft: "9em" }}>
                             {/* {item.steps} */}
                             <StyledPopconfirm
             title="Do you want to delete?"
@@ -278,9 +278,9 @@ useEffect(() => {
                  
                       <div
                        
-                        className="flex rounded-xl mt-2 bg-white h-12 items-center p-3"
+                        className="flex rounded mt-1 bg-white h-8 items-center p-1"
                       >
-                        <div className="flex font-medium flex-col md:w-[36.1rem] max-sm:w-full">
+                        <div className="flex md:w-[36.1rem] max-sm:w-full">
                           <div className="flex justify-between text-sm font-semibold font-poppins">
                             {/* {item.qualityName} */}
 
@@ -296,14 +296,14 @@ useEffect(() => {
                           </div>
                         </div>
 
-                        <div className="flex font-medium flex-col md:w-26 max-sm:justify-between w-full max-sm:flex-row">
-                          <div className="font-normal text-[0.85rem] font-poppins" style={{ marginLeft: "9em" }}>
+                        <div className="flex md:w-26 max-sm:justify-between w-full max-sm:flex-row">
+                          <div className="  text-[0.85rem] font-poppins" style={{ marginLeft: "9em" }}>
                             {item.steps}
                           </div>
                         </div>
 
-                        <div className="flex font-medium flex-col md:w-26 max-sm:justify-between w-full max-sm:flex-row">
-                          <div className="font-normal text-[0.85rem] font-poppins" style={{ marginLeft: "9em" }}>
+                        <div className="flex md:w-26 max-sm:justify-between w-full max-sm:flex-row">
+                          <div className="  text-[0.85rem] font-poppins" style={{ marginLeft: "9em" }}>
                             {/* {item.steps} */}
 
                             <MoveToggleQualityProduct 
@@ -312,14 +312,13 @@ useEffect(() => {
                                                         item={item} 
                                                         // selectedZone={selectedZone}
                                                         // selectedRack={selectedRack}
-                                                    className=' !text-icon'
-                                                        />
+                                                    className=' !text-icon' />
                           </div>
                         </div>
 
 
-                        <div className="flex font-medium flex-col md:w-26 max-sm:justify-between w-full max-sm:flex-row">
-                          <div className="font-normal text-[0.85rem] font-poppins" style={{ marginLeft: "9em" }}>
+                        <div className="flex md:w-26 max-sm:justify-between w-full max-sm:flex-row">
+                          <div className="  text-[0.85rem] font-poppins" style={{ marginLeft: "9em" }}>
                             {/* {item.steps} */}
 
    {/* <EditOutlined/> */}
@@ -336,14 +335,14 @@ useEffect(() => {
 
 
 
-                        <div className="flex font-medium flex-col md:w-26 max-sm:justify-between w-full max-sm:flex-row">
-                          <div className="font-normal text-[0.85rem] font-poppins" style={{ marginLeft: "9em" }}>
+                        <div className="flex md:w-26 max-sm:justify-between w-full max-sm:flex-row">
+                          <div className="  text-[0.85rem] font-poppins" style={{ marginLeft: "9em" }}>
                             {/* {item.steps} */}
                             <StyledPopconfirm
             title="Do you want to delete?"
             onConfirm={() => props.deleteQualityProductData(item.qualityCheckBuilderId,props.particularDiscountData.productId)}
           >
-   <DeleteOutlined/>
+   <DeleteOutlined className='!text-icon text-red-600'/>
    </StyledPopconfirm>
                           </div>
                         </div>
@@ -365,19 +364,8 @@ useEffect(() => {
 
 
 const mapStateToProps = ({ auth,location,departments,distributor,product }) => ({
-    // userId: auth.userDetails.userId,
-    // orgId:auth.userDetails.organizationId,
-    // //locationId:auth.userDetails.locationId,
-    // allLoCell:location.allLoCell,
-    // cellCode:location.cellCode,
-    // userCell:location.userCell,
     qualityProducts:product.qualityProducts
-    // departments: departments.departments,
-    // userListLocation:location.userListLocation,
-    // locationMachine:location.locationMachine,
-    // locationMachineData:location.locationMachineData
-
-});
+    });
 
 const mapDispatchToProps = (dispatch) =>
     bindActionCreators(
@@ -387,17 +375,7 @@ const mapDispatchToProps = (dispatch) =>
             addDragQuality,
             deleteQualityProductData,
             updateQualityProduct
-        //   getLocationMachine,
-        //   createMachinary,
-        //   getLocationMachineData,
-        //   createMachinaryCell
-        //     getAlLoCell,
-        //    getDepartments,
-        //    getUserListLocation,
-        //    createUserCell,
-        //    getUserCell,
-        //    getCellCode,
-        //    deleteUserCell
+        
         },
         dispatch
     );
@@ -406,98 +384,3 @@ export default connect(
     mapStateToProps,
     mapDispatchToProps
 )(QualityProductForm);
-
-// import React, { useState } from 'react';
-// import { FormattedMessage } from 'react-intl';
-// import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-
-// const initialQualityProducts = [
-//   {
-//     qualityName: "Quality A",
-//     steps: "Step 1",
-//     serialNo: "001",
-//   },
-//   {
-//     qualityName: "Quality B",
-//     steps: "Step 2",
-//     serialNo: "002",
-//   },
-//   {
-//     qualityName: "Quality C",
-//     steps: "Step 3",
-//     serialNo: "003",
-//   },
-// ];
-
-// const QualityProductsComponent = () => {
-//   const [qualityProducts, setQualityProducts] = useState(initialQualityProducts);
-
-//   const handleOnDragEnd = (result) => {
-//     if (!result.destination) return;
-
-//     const items = Array.from(qualityProducts);
-//     const [reorderedItem] = items.splice(result.source.index, 1);
-//     items.splice(result.destination.index, 0, reorderedItem);
-//     console.log(items)
-
-//     setQualityProducts(items);
-//   };
-
-//   return (
-//     <div className='flex justify-end sticky z-auto'>
-//       <div className="rounded-lg m-5 p-2 w-[96%] overflow-auto shadow-[4px_0px_9px_3px_] shadow-[#a3abb980] bg-[#eaedf1]">
-//         <div className="flex w-[95%] px-2 bg-transparent font-bold sticky top-0 z-10">
-//           <div className=""></div>
-//           <div className="md:w-[22.12rem]"><FormattedMessage id="app.step" defaultMessage="Step" /></div>
-//           <div className="md:w-[15.5rem]"><FormattedMessage id="app.serialno" defaultMessage="Serial No" /></div>
-//           <div className=""></div>
-//         </div>
-
-//         <DragDropContext onDragEnd={handleOnDragEnd}>
-//           <Droppable droppableId="qualityProducts">
-//             {(provided) => (
-//               <div {...provided.droppableProps} ref={provided.innerRef}>
-//                 {qualityProducts.map((item, index) => (
-//                   <Draggable key={item.serialNo} draggableId={item.serialNo} index={index}>
-//                     {(provided) => (
-//                       <div
-//                         ref={provided.innerRef}
-//                         {...provided.draggableProps}
-//                         {...provided.dragHandleProps}
-//                         className="flex rounded-xl mt-2 bg-white h-12 items-center p-3"
-//                       >
-//                         <div className="flex font-medium flex-col md:w-[36.1rem] max-sm:w-full">
-//                           <div className="flex justify-between text-sm font-semibold font-poppins">
-//                             {item.qualityName}
-//                           </div>
-//                         </div>
-
-//                         <div className="flex font-medium flex-col md:w-26 max-sm:justify-between w-full max-sm:flex-row">
-//                           <div className="font-normal text-[0.85rem] font-poppins" style={{ marginLeft: "9em" }}>
-//                             {item.steps}
-//                           </div>
-//                         </div>
-
-//                         <div className="flex font-medium flex-col md:w-26 max-sm:justify-between w-full max-sm:flex-row">
-//                           <div className="font-normal text-[0.85rem] font-poppins" style={{ marginLeft: "2em" }}>
-//                             {item.serialNo}
-//                           </div>
-//                         </div>
-//                       </div>
-//                     )}
-//                   </Draggable>
-//                 ))}
-//                 {provided.placeholder}
-//               </div>
-//             )}
-//           </Droppable>
-//         </DragDropContext>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default QualityProductsComponent;
-
-
-
