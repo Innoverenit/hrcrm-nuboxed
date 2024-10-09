06ce434db39000dbceda1042c20cec3050b001ed
Sangharsh <sangharshmishra16@gmail.com>
2024-10-09 17:59:10 +0530

    Sangharsh//Done

--- src/Containers/Main/Supplies/SuppliesLocationItem.js
+++ src/Containers/Main/Supplies/SuppliesLocationItem.js
@@ -38,45 +38,19 @@ const SuppliesLocationItem = (props) => {
                 <div class="rounded m-1 p-1 w-[100%]  overflow-auto shadow-[4px_0px_9px_3px_] shadow-[#a3abb980] bg-[#eaedf1]">
                     <div className=" flex  w-[100%]  p-1 bg-transparent font-bold sticky z-10">
                         <div className=""></div>
-                        <div className=" w-[15.5rem]"><FormattedMessage id="app.po" defaultMessage="PO ID" /></div>
+         
                         <div className=" w-[13.52rem]">
                             {/* <FormattedMessage id="app.created" defaultMessage="Created" /> */}
                          Item Name
                         </div>
                         <div className=" w-[11.122rem]">       
-                       Po Id
+                    Balance
 
                         </div>
-                        <div className=" w-[11.122rem]">       
-                       Supplier Name
+                      
 
-                        </div>
-
-                        <div className=" w-[11.122rem]">       
-                      HSN
-
-                        </div>
-
-                        <div className=" w-[11.122rem]">       
-                      Supplies Id
-
-                        </div>
-                        {/* <div className=" w-[11.122rem]">       
-                      Repacked
-
-                        </div>
-                        <div className=" w-[11.122rem]">       
-                     Credit Note
-
-                        </div> */}
-                        <div className=" w-[11.122rem]">       
-                    Units
-
-                        </div>
-                        <div className=" w-[11.122rem]">       
-                    Terms and Condition
-
-                        </div>
+                     
+                   
 
                         <div className=" w-[11.322rem]"></div>
                     </div>
@@ -88,53 +62,23 @@ const SuppliesLocationItem = (props) => {
                         height={"67vh"}
                         style={{ scrollbarWidth:"thin"}}
                     > */}
-                        {/* {props.materialReceiveData.map((item) => {
+                     {props.suppliesLocationItem.map((item) => {
                             const currentdate = dayjs().format("DD/MM/YYYY");
                             const date = dayjs(item.creationDate).format("DD/MM/YYYY");
                             return (
                                 <div>
                                     <div className="flex rounded  mt-1 bg-white h-8 items-center p-1 ">
-                                        <div class="flex">
-
-                                            <div className=" flex  w-[16.1rem] max-sm:w-full  ">
-                                                <div class="flex justify-between text-xs font-bold  font-poppins cursor-pointer underline text-blue-600">
-                                                    <div
-                                                        onClick={() => {
-                                                            handleRow(item);
-                                                            props.handleMaterialReceived(true);
-                                                        }}
-                                                    >
-                                                        {item.newPoNumber}
-                                                    </div>
-                                                    {date === currentdate ? (
-                                                        <div class="text-xs font-poppins font-bold text-[tomato]">
-                                                          {props.translatedMenuItems[13]}  
-                                                         
-                                                        </div>
-                                                    ) : null}
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className=" flex w-[4.12rem] max-sm:flex-row  max-sm:justify-between  ">
-
-                                            <div class=" text-xs  font-poppins">
-                                                <MultiAvatar
-                                                    primaryTitle={item.userName}
-                                                    imgWidth={"1.8rem"}
-                                                    imgHeight={"1.8rem"}
-                                                />
-                                            </div>
-
-                                        </div>
+                                      
+                                    
                                         <div className=" flex  w-[8.32rem] max-sm:flex-row  max-sm:justify-between  ">
 
-                                            {date}
+                                            {item.suppliesFullName}
 
                                         </div>
                                         <div className=" flex   w-[10.22rem] max-sm:flex-row  max-sm:justify-between  ">
 
                                             <div class=" text-xs  font-poppins">
-                                                {item.supplierName}
+                                                {item.balanced}
                                             </div>
                                         </div>
                                       
@@ -146,7 +90,7 @@ const SuppliesLocationItem = (props) => {
 
                                 </div>
                             );
-                        })} */}
+                        })} 
                     {/* </InfiniteScroll> */}
                 </div>
             </div>
