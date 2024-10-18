dbaa0dd59b4bbad5bc9d1231a9a959dcc730a5a1
Sangharsh <sangharshmishra16@gmail.com>
2024-10-18 18:34:35 +0530

    Sangharsh//Done

--- src/Containers/Dashboard/Child/DashBoardSummary.js
+++ src/Containers/Dashboard/Child/DashBoardSummary.js
@@ -1,20 +1,10 @@
 import React, { useEffect, useState} from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import HourglassEmptyIcon from '@mui/icons-material/HourglassEmpty'
-import HourglassTopIcon from '@mui/icons-material/HourglassTop';  
-import HourglassBottomIcon from '@mui/icons-material/HourglassBottom'
-import {Button,Tooltip} from "antd"
-import dayjs from "dayjs";
 
-import {
-  getMaterialBestBefore,
+import { getQuotationDashboard,getQuotationDashboardCount } from "../../Dashboard/DashboardAction";
 
-} from "../../Main/Inventory/InventoryAction";
 
-import { getQuotationDashboard,getReorderdata,getQuotationDashboardCount ,getTakskdashboardGantt,getTasklist} from "../../Dashboard/DashboardAction";
-
-const ButtonGroup = Button.Group;
 const DashBoardSummary=(props) =>{
 
 
@@ -23,10 +13,6 @@ const DashBoardSummary=(props) =>{
 
     props.getQuotationDashboard(props.userId);
     props.getQuotationDashboardCount(props.userId)
-    props.getMaterialBestBefore(props.locationId)
-    props.getTakskdashboardGantt(props.userId)
-    props.getTasklist(props.userId)
-    props.getReorderdata()
 
 
    
@@ -66,146 +52,53 @@ const DashBoardSummary=(props) =>{
   };
 
   return (
-    <div className="container mx-auto p-4 grid grid-cols-5 gap-6  rounded mt-1  bg-white h-[87vh] ">
+    <div className="container mx-auto p-4 grid grid-cols-4 gap-6  rounded mt-1  bg-white h-[87vh] ">
       {/* TASK */}
       <div class="flex flex-col">
-        <h2 className="text-xl font-bold mb-4">TASK-({`${props.taskperCount.totalTask} `})</h2>
-        <div className="overflow-y-auto max-h-96">
-        {props.tasksdashboardGantt.map((deal, index) => (
+        <h2 className="text-xl font-bold mb-4">TASK-(5)</h2>
+        {data.deals.map((deal, index) => (
           <div key={index} className="mb-4 p-2 box-content border-2 border-[#00008b23] ">
             <div className="flex justify-between">
               <div>
-                <p className="font-semibold">{deal.taskName}</p>
-                <p className="text-sm text-gray-500">
-                <ButtonGroup >
-         
-         <StatusIcon class=" !text-icon"
- type="To Start"
- iconType={<HourglassEmptyIcon  className=" !text-icon" />} 
-// iconType="fa-hourglass-start"
- tooltip="To Start"
-//  status={item.taskStatus}
-//  difference={difference} 
-//  onClick={() =>
-//    linkTaskStatus(item.taskId, {
-//      taskStatus: "To Start",
-//    })
-//  }
-/>
-       
-           <StatusIcon class=" !text-icon"
-             type="In Progress"
-            iconType={<HourglassTopIcon  className=" !text-icon"/>}
-             tooltip="In Progress"
-            //  status={item.taskStatus}
-            //  difference={difference}
-            //  onClick={() =>
-            //    linkTaskStatus(item.taskId, {
-            //      //  ...item,
-            //       taskStatus: "In Progress",
-            //    })
-            //  }
-           />
-        
-           <StatusIcon class=" !text-icon"
-             type="Completed"
-           iconType={<HourglassBottomIcon  className=" !text-icon"/>}
-             tooltip="Completed"
-            //  status={item.taskStatus}
-            //  difference={difference}
-            //  onClick={() =>
-            //    linkTaskStatus(item.taskId, {
-            //      //  ...item,
-            //       taskStatus: "Completed",
-            //    })
-            //  }
-           />
-         
-       </ButtonGroup>
-                  
-                  
-                  
-                  </p>
+                <p className="font-semibold">{deal.name}</p>
+                <p className="text-sm text-gray-500">{deal.description}</p>
               </div>
-              <p className="text-red-600 font-bold bg-red-100 inline-block px-2 py-1 rounded">
-              {`${dayjs(deal.endDate).format("DD/MM/YYYY")}`}
-                </p>
+              <p className="text-sm text-gray-500">{deal.date}</p>
             </div>
-            {/* <p className="text-green-600 font-bold bg-green-100 inline-block px-2 py-1 rounded">${deal.amount}</p> */}
+            <p className="text-green-600 font-bold bg-green-100 inline-block px-2 py-1 rounded">${deal.amount}</p>
           </div>
         ))}
-        </div>
       </div>
 {/* <div class="relative   sm:after:content-[''] sm:after:absolute sm:after:w-[3px] sm:after:min-h-[500vh] sm:after:bg-black sm:after:right-[-1rem]"></div> */}
       {/* QUOTATION */}
-      <div class="flex flex-col border-x-black h-[40vh]">
-  <h2 className="text-xl font-bold mb-4 ">
-  QUOTATION -
-  ({`${props.quotationDashboardCount.countByUserId} `})
+      {/* <div class=" border-l-2 box-border border-black"></div> */}
+      <div class="flex flex-col border-l-2 box-border border-black">
+  <h2 className="text-xl font-bold mb-4 ml-2">
+  QUOTATION - (1)
+  {/* ({`${props.quotationDashboardCount.} `}) */}
 </h2>
   {props.quotationDashboard.length === 0 ? (
     <p>No data found</p>
   ) : (
     props.quotationDashboard.map((lead, index) => (
-      <div key={index} className="mb-4 p-2 box-content border-2 border-[#00008b23]">
+      <div key={index} className="mb-4 p-2 box-content border-2 border-[#00008b23] ml-2">
         <div className="flex justify-between">
           <p className="font-semibold">{lead.quotationId}</p>
           <p className="text-sm text-gray-500">{lead.time}</p>
-          
         </div>
         <p className="text-sm text-gray-500">{lead.amount}</p>
         <p className="text-sm text-gray-500">{lead.contactPersonName}</p>
-        <Button>To Order</Button>
       </div>
     ))
   )}
 </div>
 
 
-    
-
-  {/* Re Order */}
-  <div class="flex flex-col">
-        <h2 className="text-xl font-bold mb-4"> Re Order-(0) </h2>
-        {props.reOrderData.map((colleague, index) => (
-          <div key={index} className="mb-4 p-2 box-content border-2 border-[#00008b23]">
-            <div className="flex justify-between">
-              <p className="font-semibold">{colleague.suppliesFullName} {colleague.batchNo}</p>
-              <p className="text-red-600 font-bold bg-red-100 inline-block px-2 py-1 rounded">
-             {colleague.reorderLevel}
-                </p>
-            </div>
-            <p className="text-sm text-gray-500">{colleague.zone} {colleague.aisle} {colleague.chamber} </p>
-            <p className="text-sm text-gray-500">{colleague.locationName}</p>
-            <Button>Create PO</Button>
-          </div>
-        ))}
-      </div>
-
-         {/* Best Before */}
-         <div class="flex flex-col">
-        <h2 className="text-xl font-bold mb-4"> Best Before-(2) </h2>
-        {props.materialBestBefore.map((colleague, index) => (
-          <div key={index} className="mb-4 p-2 box-content border-2 border-[#00008b23]">
-            <div className="flex justify-between">
-              <p className="font-semibold">{colleague.suppliesFullName} {colleague.batchNo}</p>
-              <p className="text-red-600 font-bold bg-red-100 inline-block px-2 py-1 rounded">
-              {`${dayjs(colleague.bestBeforeUse).format("DD/MM/YYYY")}`}
-                </p>
-            </div>
-            <p className="text-sm text-gray-500">{colleague.newPoNumber} {colleague.hsn} </p>
-                      <p className="text-sm text-gray-500">{colleague.zone} {colleague.aisle} {colleague.chamber} </p>
-            <Button>To Waste</Button>
-          </div>
-        ))}
-      </div>
-
-
-        {/* ORDER */}
-        <div class="flex flex-col">
-        <h2 className="text-xl font-bold mb-4">ORDER-(6)</h2>
+      {/* ORDER */}
+      <div class="flex flex-col border-l-2 box-border border-black">
+        <h2 className="text-xl font-bold mb-4 ml-2 ">ORDER-(6)</h2>
         {data.notInCrm.map((contact, index) => (
-          <div key={index} className="mb-4 p-2 box-content border-2 border-[#00008b23]">
+          <div key={index} className="mb-4 p-2 box-content border-2 border-[#00008b23] ml-2">
             <div className="flex justify-between">
               <p className="font-semibold">{contact.name}</p>
               <p className="text-sm text-gray-500">{contact.time}</p>
@@ -217,16 +110,13 @@ const DashBoardSummary=(props) =>{
       </div>
 
       {/* DEALS */}
-      <div class="flex flex-col">
-        <h2 className="text-xl font-bold mb-4"> DEALS-(5) </h2>
+      <div class="flex flex-col border-l-2 box-border border-black">
+        <h2 className="text-xl font-bold mb-4 ml-2"> DEALS-(5) </h2>
         {data.colleagues.map((colleague, index) => (
-          <div key={index} className="mb-4 p-2 box-content border-2 border-[#00008b23]">
+          <div key={index} className="mb-4 p-2 box-content border-2 border-[#00008b23] ml-2">
             <div className="flex justify-between">
-              <p className="font-semibold">{colleague.name}  </p>
-              <p className="text-sm text-gray-500">
-                {colleague.date}
-                
-                </p>
+              <p className="font-semibold">{colleague.name}</p>
+              <p className="text-sm text-gray-500">{colleague.date}</p>
             </div>
             <p className="text-sm text-gray-500">{colleague.description}</p>
           </div>
@@ -235,56 +125,16 @@ const DashBoardSummary=(props) =>{
     </div>   
   );
 };
-const mapStateToProps = ({ dashboard,inventory, auth }) => ({
+const mapStateToProps = ({ dashboard, auth }) => ({
   userId: auth.userDetails.userId,
-  locationId: auth.userDetails.locationId,
-  reOrderData:dashboard.reOrderData,
-  taskperCount:dashboard.taskperCount,
-  tasksdashboardGantt:dashboard.tasksdashboardGantt,
-  materialBestBefore:inventory.materialBestBefore,
   quotationDashboard:dashboard.quotationDashboard,
   quotationDashboardCount:dashboard.quotationDashboardCount
 });
 const mapDispatchToProps = (dispatch) => bindActionCreators({
   getQuotationDashboard,
-  getQuotationDashboardCount,
-  getMaterialBestBefore,
-  getTasklist,
-  getReorderdata,
-  getTakskdashboardGantt
+  getQuotationDashboardCount
 
 }, dispatch);
 export default connect(mapStateToProps, mapDispatchToProps)(DashBoardSummary);
-function StatusIcon(props) {
-  const { type, iconType, tooltip, status, onClick, difference } = props;
 
-  let iconColor = status === type ? "rgb(251, 133, 0)" : "grey";
-  let size = status === type ? "1.875em" : "1em";
 
-  // Display the difference as a label next to the icon
-  const daysLabel = difference > 0 ? `+${difference} days` : `${difference} days`;
-
-  return (
-    <Tooltip title={`${tooltip} (${daysLabel})`}>
-      <Button
-        ghost={status !== type}
-        style={{
-          padding: "0.375em",
-          borderColor: "transparent",
-          color: iconColor,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-        onClick={onClick}
-      >
-        {/* <i className={`fas ${iconType}`} style={{ fontSize: "1.375em" }} /> */}
-        {iconType}
-        {/* <HourglassEmptyIcon/> */}
-{/* 
-        {status === type && <span style={{ fontSize: "0.82rem",display:"flex" }}>{daysLabel}</span>} */}
-     
-      </Button>
-    </Tooltip>
-  );
-}
