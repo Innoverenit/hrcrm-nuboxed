28f729f73fd50f02e8972c4d17d69aa5e5bc7880
Itisri <itisrichaudhury236>
2024-10-24 00:05:06 +0530

    iti

--- src/Containers/Contact/Child/ContactTable/AddContactAddressDrawerModal.js
+++ src/Containers/Contact/Child/ContactTable/AddContactAddressDrawerModal.js
@@ -30,9 +30,9 @@ class AddContactAddressDrawerModal extends Component {
           <AddressData
           uniqueId={this.props.item.contactId}
           type={this.props.type}
-          translateText={props.translateText}
-        selectedLanguage={props.selectedLanguage}
-      translatedMenuItems={props.translatedMenuItems}
+          translateText={this.props.translateText}
+        selectedLanguage={this.props.selectedLanguage}
+      translatedMenuItems={this.props.translatedMenuItems}
           />
 
 
