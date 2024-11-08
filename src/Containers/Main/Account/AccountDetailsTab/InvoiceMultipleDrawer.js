5d6937c65c569e9e92915559eafe09955961c651
Dibya391 <dibyaranjan828@gmail.com>
2024-11-08 19:42:34 +0530

    drb3

---
+++ src/Containers/Main/Account/AccountDetailsTab/InvoiceMultipleDrawer.js
@@ -0,0 +1,33 @@
+import React, { Suspense } from "react";
+import { BundleLoader } from "../../../../Components/Placeholder";
+import { StyledDrawer } from "../../../../Components/UI/Antd";
+import InvoiceMultipleDrawerCard from "./InvoiceMultipleDrawerCard";
+
+function InvoiceMultipleDrawer (props) {
+  return (
+    <>
+      <StyledDrawer
+        title={`${props.particularRowData.invoiceId}`}
+        width="60%"
+        visible={props.openMultipleDrawer}
+        maskClosable={false}
+        destroyOnClose
+        onClose={() => props.setopenMultipleDrawer(false)}
+        footer={null}
+      >
+        <Suspense fallback={<BundleLoader />}>
+        <InvoiceMultipleDrawerCard 
+        particularRowData={props.particularRowData}
+        openMultipleDrawer={props.openMultipleDrawer}
+         setopenMultipleDrawer={props.setopenMultipleDrawer}
+         distributorId={props.distributorId}
+         selectedLanguage={props.selectedLanguage}
+         translateText={props.translateText} 
+        /> 
+        </Suspense>
+      </StyledDrawer>
+    </>
+  );
+};
+
+export default InvoiceMultipleDrawer;
