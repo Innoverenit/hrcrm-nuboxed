23b7f5dae3b2642bf7250378d0bc69da246fab98
Lapi <srjjack9@gmail.com>
2024-11-11 18:31:31 +0530

    Lapi//done

--- src/Containers/Main/Inventory/InventoryWastetab.js
+++ src/Containers/Main/Inventory/InventoryWastetab.js
@@ -39,11 +39,130 @@ const InvenoryWastetab = (props) => {
  
 
    
-console.log(selectedZones)
+console.log(selectedLocation)
 
    
     return (
         <>
+ {selectedLocation ?
+ <>
+  <div className=' flex sticky  z-auto h-[79vh]'>
+                <div class="rounded m-1 p-1 w-[100%]  overflow-auto shadow-[4px_0px_9px_3px_] shadow-[#a3abb980] bg-[#eaedf1]">
+                <div className=" flex  w-[100%]  p-1 bg-transparent font-bold font-poppins text-xs items-end sticky z-10">
+                        <div className=""></div>
+                        <div className="text-[#00A2E8] text-base w-[19.5rem]"><FormattedMessage id="app.po" defaultMessage="PO ID" /></div>
+                        <div className=" w-[11.52rem]" >
+                            {/* <FormattedMessage id="app.created" defaultMessage="Created" /> */}
+                         Item 
+                        </div>
+             
+                       
+               
+                        <div className=" w-[12.122rem]" >       
+                        <FactoryIcon className='!text-base  text-[#e4eb2f]' />   Supplier Name
+                        </div>
+
+                        <div className=" w-[4.122rem]">       
+                      HSN
+                        </div>
+
+                        <div className=" w-[8.12rem]">       
+                        <CategoryIcon className='!text-base  text-[#e4eb2f]'/> Supplies Id
+
+                        </div>    
+                        <div className=" w-[5.12rem]" >       
+                   Country
+
+                        </div>
+                        <div className=" w-[8.12rem]" >       
+                   Best Use Date
+
+                        </div>
+
+                        <div className=" w-[5.12rem]" >       
+                    Units
+
+                        </div>
+                        <div className=" w-[9.2rem]" >       
+                    Zone
+
+                        </div>
+                        <div className=" w-[13.2rem]">       
+                    Aisle
+
+                        </div>
+                        <div className=" w-[7.12rem]">       
+                    Rack
+
+                        </div>
+                   
+
+                        <div className=" w-[3.22rem]"></div>
+                    </div>
+                  
+                         {props.westMaterialLocation.map((item,index) => {
+                            const currentdate = dayjs().format("DD/MM/YYYY");
+                            const date = dayjs(item.creationDate).format("DD/MM/YYYY");
+                            return (
+                                <div>
+                                    <div className="flex rounded py-1 mt-1 bg-white  items-center  scale-[0.99] hover:scale-100 ease-in duration-100 shadow  border-solid   leading-3 hover:border  hover:border-[#23A0BE]  hover:shadow-[#23A0BE] ">
+                                      
+
+                                            <div className=" flex items-center border-l-2  h-8 border-green-500 bg-[#eef2f9] w-[10.1rem] max-sm:w-full ">
+                                                <div class="flex ml-gap text-xs font-bold  font-poppins cursor-pointer underline text-blue-600 border-l-2 border-green-500 bg-[#eef2f9]">
+                                                    <div
+                                                      
+                                                    >
+                                                        {item.newPoNumber}
+                                                    </div>
+                                                    
+                                                </div>
+                                            </div>
+                                     
+
+                                        <div className=" flex w-[18.12rem] items-center h-8 ml-gap bg-[#eef2f9] max-sm:flex-row  max-sm:justify-between  ">
+
+                                            <div class=" text-xs  ml-gap font-poppins" >
+                                               
+                                                {item.suppliesFullName}  
+                                            </div>
+
+                                        </div>
+
+                                       
+                                        <div className=" flex w-[11.02rem] items-center h-8 ml-gap bg-[#eef2f9] max-sm:flex-row  max-sm:justify-between  ">
+                                        <div className="  text-xs  ml-gap font-poppins " >
+
+                                            {item.supplierName}
+
+                                        </div>
+                                        </div>
+                                        <div className=" flex  items-center justify-start h-8 ml-gap bg-[#eef2f9] w-[4.25rem] max-sm:flex-row  max-sm:justify-between  ">
+
+                                            <div class=" text-xs ml-gap font-poppins">
+                                                {item.hsn}
+                                            </div>
+                                        </div>
+                                      
+                                        <div className=" flex  md:w-[6.4rem] items-center justify-start h-8 ml-gap bg-[#eef2f9] max-sm:flex-row w-full max-sm:justify-between ">
+                                        <div class=" text-xs ml-gap font-poppins">
+                                        {item.newSuppliesNo}
+                                            </div>
+                                          
+                                        </div>
+                                       
+
+                                                              
+                                    </div>
+
+                                </div>
+                            );
+                        })} 
+                 
+                </div>
+            </div>
+ </>
+:
             <div className=' flex sticky  z-auto h-[79vh]'>
                 <div class="rounded m-1 p-1 w-[100%]  overflow-auto shadow-[4px_0px_9px_3px_] shadow-[#a3abb980] bg-[#eaedf1]">
                 <div className=" flex  w-[100%]  p-1 bg-transparent font-bold font-poppins text-xs items-end sticky z-10">
@@ -181,6 +300,8 @@ console.log(selectedZones)
                     {/* </InfiniteScroll> */}
                 </div>
             </div>
+        }
+
             <TermsnConditionModal
                 rowData={row}
                 addTermsnCondition={props.addTermsnCondition}
